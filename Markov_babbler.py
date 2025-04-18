9e67226c756a4dd91f90bf2901c06ea4214c363a
ShadowTheLegendary <shadow2493478665@gmail.com>
2025-04-18 09:14:24 -0400

    Add project files.

---
+++ Markov_babbler.py
@@ -0,0 +1,59 @@
+import os
+import random
+import json
+
+def is_end(word):
+    if word[-1] in ['.', '!', '?']:
+        return True
+    return False
+
+def get_stats(str = "And this and that."):
+    words = str.split()
+    procesed_words = []
+    stats = {}
+    for word in words:
+        procesed_words.append(word)
+    
+    for i in range(len(procesed_words) - 1):
+        if procesed_words[i] in stats.keys():
+            stats[procesed_words[i]].append(procesed_words[i+1])
+        else:
+            stats[procesed_words[i]] = [procesed_words[i+1]]
+    return stats
+
+def babble(stats, sentances):
+    result = ""
+    for i in range(sentances):
+        word = random.choice(list(stats.keys()))
+        sentance = word.capitalize()
+        while not is_end(word):
+            word = random.choice(stats[word])
+            sentance += " " + word
+        result += sentance + " "
+    return result
+
+def user_interface():
+    while True:
+        os.system("cls")
+        print("Welcome to the Markov babbler!")
+        filepath = input("What file do you want to train the model on: ")
+        try:
+            file = open(filepath + ".txt", "r", encoding="utf-8")
+            text = file.read()
+            file.close()
+        except:
+            print(f"I cant find: {filepath}.txt")
+            choice = input()
+            if choice == "quit":
+                return
+            continue
+        stats = get_stats(text)
+        sentances = input("How many sentances of babble do you want: ")
+        print(babble(stats, int(sentances)))
+        choice = input()
+        if choice == "quit":
+            return
+
+def test():
+    file = open("1984_Stats.json", 'r')
+    print(babble(json.load(file), 10))
